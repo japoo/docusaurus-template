afac1cd12f61f4137b3eb7db69913a95170079a4
Pooja <pooja.rajeshlingam@gmail.com>
2021-08-19 10:53:58 +0530

    Update docusaurus.config.js
    added code for Print in nav bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,17 @@ module.exports = {
         alt: 'Masterworks',
         src: 'img/Logo.png',
       },
-      items: [],
+      items: [
+      {
+          href: '#',
+          className: 'forprint',
+          // Only one of "to" or "href" should be used
+          // href: 'https://www.facebook.com',
+          label: 'Print',
+          position: 'right',
+          //activeBaseRegex: 'docs/(next|v8)',
+        },
+      ],
     },
     footer: {
       style: 'dark',
